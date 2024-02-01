f36f653f4b260f14d2946ec3f9d5816c3b1f8070
Peter Blomqvist <peterblomkvist@hotmail.com>
2024-02-01 21:42:45 +0100

    Checkoutpage works

--- DigiDepot/src/pages/Checkout.jsx
+++ DigiDepot/src/pages/Checkout.jsx
@@ -1,18 +1,30 @@
-import React, { useContext } from "react";
-import Sidebar from "../components/Sidebar";
-import { SidebarContext } from "../contexts/SideBarContext";
+import { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
+import CartItem from '../components/CartItem';
 
 const Checkout = () => {
-  const { isOpen, handleClose } = useContext(SidebarContext);
 
-  // Här tilldelar du värdet true till isOpen
-  // const isOpen = true; <-- Detta behöver inte vara här, eftersom du redan destrukturerar isOpen från useContext.
+    const { cart, clearCart, total, itemQuantity } = useContext(CartContext);
 
-  return (
-    <div>
-      <Sidebar />
-    </div>
-  );
+
+    return (
+        <div>
+            <div className='sidebarCheckout'>
+                <div className="shoppingCartCheckout">Checkout: {`(${itemQuantity})`}</div>
+                <div>{cart.map((item) => (
+                    <CartItem item={item} key={item._id} />
+                ))}</div>
+                <div className='sidebarBottom'>
+                    <div className='sidebarTotalText'>
+                        <span>Total:</span>{total}kr
+                    </div>
+                    <button className='btnCheckout'>Click here to confirm your order!</button>
+                    <div><i onClick={clearCart} className="fa-solid fa-trash-can"></i></div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Checkout;
+
