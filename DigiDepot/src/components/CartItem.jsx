0a82cf534c965d92f06ee357233a4f5d4dfecd5e
Peter Blomqvist <peterblomkvist@hotmail.com>
2024-02-01 11:52:05 +0100

    added text for shoppingcart

--- DigiDepot/src/components/CartItem.jsx
+++ DigiDepot/src/components/CartItem.jsx
@@ -14,9 +14,20 @@ const CartItem = ({ item }) => {
   return (
     <div className="container">
       <div className="cartitem">
-        <Link>
+        <Link to={`/productdetail/${_id}`}>
           <img className="cartimg" src={images[0]} alt=""/>
         </Link>
+        <div>
+          <div>
+          <Link to={`/productdetail/${_id}`}  className="carttext">{name}</Link>
+          </div>
+          <div><i className="fa-solid fa-circle-xmark"></i></div>
+          <div className="cartprices">
+              <div>quantity</div>
+              <div>item price</div>
+              <div>final price</div>
+          </div>
+        </div>
       </div>
     </div>
   )
